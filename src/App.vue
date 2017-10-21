711b0cb02f845a77072e032f32dc5b4218ad9c20
xpavlicek <xpavlicek@gmail.com>
2017-10-21 22:01:42 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <top-panel></top-panel>
+
+    <div>ABC</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import TopPanel from './components/TopPanel.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TopPanel
+  }
+
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma/bulma.sass';
+/*
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}*/
+</style>
